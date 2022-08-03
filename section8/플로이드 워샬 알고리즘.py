074192c94125f07abc187885da03ec2bae1ed2d8
dlrkdmsdkdld <78574719+dlrkdmsdkdld@users.noreply.github.com>
2022-08-03 22:49:33 +0900

    day53

---
+++ section8/플로이드 워샬 알고리즘.py
@@ -0,0 +1,24 @@
+
+
+n,m=map(int,input().split())
+data=[[5000]*(n+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    data[i][i]=0
+for i in range(m):
+    x,y,z=map(int,input().split())
+    data[x][y]=z
+for k in range(1,n+1):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            data[i][j] = min(data[i][j],data[i][k]+data[k][j])
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if data[i][j]==5000:
+            print("M",end=' ')
+        else:
+            print(data[i][j],end=' ')
+    print()
+
+
+
+
